ba9ddc98ef5b26699668096f3c4c785dd42686cb
Ian Valentino <130348417+ivrequenajerez@users.noreply.github.com>
2024-05-19 13:54:16 +0200

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#FFF7EF",
+          100: "#FFF1E0",
+          200: "#FFE8CC",
+        },
+        secondary: {
+          DEFAULT: "#D68C45",
+          100: "#E4A368",
+          200: "#F1B98C",
+        },
+        dark: {
+          DEFAULT: "#524439",
+          100: "#726357",
+          200: "#938275",
+        },
+        success: {
+          DEFAULT: "#33B174",
+          100: "#66C994",
+          200: "#99E1B4",
+        },
+        warning: {
+          DEFAULT: "#7B3100",
+          100: "#995840",
+          200: "#B78080",
+        },
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
