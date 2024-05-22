44f067cdbf16c19566c8df646d2dbd4aaef9701f
Ian Valentino <130348417+ivrequenajerez@users.noreply.github.com>
2024-05-22 13:25:42 +0200

    Subir foto y vídeo, ver posts en home y perfil

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -7,7 +7,7 @@ import CustomButton from "../../components/CustomButton";
 import { Video, ResizeMode } from "expo-av";
 import { icons } from "../../constants";
 
-import * as DocumentPicker from "expo-document-picker";
+import * as ImagePicker from "expo-image-picker";
 import { router } from "expo-router";
 
 import {createVideo} from '../../lib/appwrite';
@@ -26,11 +26,10 @@ const Create = () => {
 
   const openPicker = async (selectType) => {
     // Subir archivos de móvil y guardarlos en la BBDD, además de convertirlos en una URL
-    const result = await DocumentPicker.getDocumentAsync({
-      type:
-        selectType === "image"
-          ? ["image/png", "image/jpg", "image/jpeg"]
-          : ["video/mp4", "video/gif"],
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: selectType === 'image' ? ImagePicker.MediaTypeOptions.Images : ImagePicker.MediaTypeOptions.Videos,
+      aspect: [4, 3],
+      quality: 1,
     });
 
     if (!result.canceled) {
